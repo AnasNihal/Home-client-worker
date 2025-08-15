086d47e707e0c4b9bdcbea9550a4770dcc3591f4
AnasNihal <anasnihal03@gmail.com>
2025-08-15 22:25:36 +0530

    Make some changes and django admin

--- back-end/HomeApp/admin.py
+++ back-end/HomeApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserProfile,ServiceCategory,Booking
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(ServiceCategory)
+admin.site.register(Booking)
